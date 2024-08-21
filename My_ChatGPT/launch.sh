4b242a5f84bafcbbf178d27737ecf006c7bca168
keyvan-Re <111032743+keyvan-Re@users.noreply.github.com>
2024-08-21 16:48:07 +0330

    Update launch.sh

--- My_ChatGPT/launch.sh
+++ My_ChatGPT/launch.sh
@@ -1,5 +1,5 @@
 export OPENAI_API_KEY=
-python /content/LLMs_Project/My_ChatGPT/cli_llamaindex.py \
+python /content/LLMs_Project/My_ChatGPT/chatgpt_CMD.py \
     --memory_basic_dir ../memories \
     --memory_file /content/LLMs_Project/memory_bank/update_memory.json \
     --language en
